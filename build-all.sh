a2852dbc278b6c8489f47807ef070f817a5e5548
Death <death@death-dev-desktop.(none)>
2012-02-02 14:19:38 -0600

    Resolved remaining errors with getting everything to build correctly when the unit tests pass.

--- build-all.sh
+++ build-all.sh
@@ -6,13 +6,20 @@ if [ "$CC" = "" ]; then
   CC=gcc
 fi
 
+BUILD_APPS=0
+
 echo "Building Unit Tests"
+
 ./build-unit-test.sh $CC
 ./run-unit-test.sh
-BUILD_APPS=0
 UNIT_TESTS_PASSED=$?
 
-if [ $UNIT_TESTS_PASSED -eq 0 ]; then
+echo "UNIT_TESTS_PASSED: $UNIT_TESTS_PASSED"
+
+#if [ $UNIT_TESTS_PASSED -eq 0 ]; then
+if (( $UNIT_TESTS_PASSED )); then
+  BUILD_APPS=1
+else
   echo "Unit tests have failed. Do you want to proceed with building? (Y/N)"
   PROMPT_RESULT="N"
   read -t 10 PROMPT_RESULT
@@ -23,8 +30,6 @@ if [ $UNIT_TESTS_PASSED -eq 0 ]; then
     echo "ABORTING BUILD!!!!"
     BUILD_APPS=0
   fi
-else
-  BUILD_APPS=1
 fi
 
 #Performs the actual building of apps
@@ -32,4 +37,5 @@ if (( $BUILD_APPS )); then
   echo "Building Applications"
   ./build-dvt.sh $CC
   ./build_debug_app_linux.sh $CC
+  echo "Build Complete!"
 fi
