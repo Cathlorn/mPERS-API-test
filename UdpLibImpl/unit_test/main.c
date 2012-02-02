70048f28eb78175e6dd703fcad423c23153c4854
Death <death@death-dev-desktop.(none)>
2012-02-02 13:45:42 -0600

    Fixed where the return value of the udp unit test was not consistent with the unit test return scheme for myHalo UDP. This was causing the build-all script to fail on passing unit test results.

--- UdpLibImpl/unit_test/main.c
+++ UdpLibImpl/unit_test/main.c
@@ -1,7 +1,20 @@
 #include "test_mode.h"
 
+#define SUCCESS  0
+#define FAIL    -1
+
 int main ( int argc, char *argv[] )
 {
+    int success = FAIL;
 
-    return udp_test();
+    if(udp_test())
+    {
+        success = SUCCESS;
+    }
+    else
+    {
+        success = FAIL;
+    }
+
+    return success;
 }
